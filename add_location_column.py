840befef33fe3aad78d8c1e82226d21b5f767872
ranjanjyoti152 <ranjanjyoti152@gmail.com>
2025-04-22 17:51:27 +0530

    Remove unused script files

--- add_location_column.py
+++
@@ -1,8 +0,0 @@
-from app import app, db
-from sqlalchemy import text
-
-with app.app_context():
-    with db.engine.connect() as conn:
-        conn.execute(text("ALTER TABLE camera ADD COLUMN location VARCHAR(255)"))
-        conn.commit()
-    print("Added location column to camera table")
